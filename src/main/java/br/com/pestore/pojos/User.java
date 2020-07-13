dfb1628210fd1d05e68b8f2adaed1347395091b8
Carlos Almeida <60942979+carlosteixeira89@users.noreply.github.com>
2020-07-13 01:10:53 -0300

    Criado Pojo User

---
+++ src/main/java/br/com/pestore/pojos/User.java
@@ -0,0 +1,78 @@
+package br.com.pestore.pojos;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+
+	@SerializedName("id")
+	private Integer id;
+	@SerializedName("username")
+	private String username;
+	
+	@SerializedName("firstName")
+	private String firstName;
+	
+	@SerializedName("lastName")
+	private String lastName;
+	
+	@SerializedName("email")
+	private String email;
+	
+	@SerializedName("phone")
+	private String phone;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	
+	
+	
+
+}
